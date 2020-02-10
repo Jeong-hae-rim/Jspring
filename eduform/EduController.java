5be75089cb8d07bdbe0598fc4202fb4b252c61d5
semi <58544957+stella-semimin@users.noreply.github.com>
2020-02-10 11:20:24 +0900

    Create EduController.java
    컨트롤러 구현

---
+++ eduform/EduController.java
@@ -0,0 +1,52 @@
+package my.spring.springedu;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.omg.CORBA.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class EduController {
+	@Autowired
+	ServletContext context;
+
+	public EduController() {
+		System.out.println("EduController Create object");
+	}
+
+	@RequestMapping(value = "/eduform1", method = {RequestMethod.GET, RequestMethod.POST})
+	 
+		public ModelAndView myPost( 
+				@RequestParam("myid") String id,
+			    @RequestParam("score") int score) {
+		    ModelAndView mav = new ModelAndView();
+		    mav.addObject("myid", id);
+			switch(score/10) {
+			case 10:
+			case 9: 
+				mav.setViewName("gradeA");
+				break;
+			case 8:
+				mav.setViewName("gradeB");
+				break;
+			case 7:
+				mav.setViewName("gradeC");
+				break;
+			default:
+				mav.setViewName("gradeD");
+				break;
+			}
+			
+			return mav;
+		}
+	
+
+}
