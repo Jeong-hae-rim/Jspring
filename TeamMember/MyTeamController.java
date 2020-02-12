6adcf1b5b39035d440a876af5797d7264174fa9c
Jeong-hae-rim <58800295+Jeong-hae-rim@users.noreply.github.com>
2020-02-12 11:01:42 +0900

    2020.02.12
    teammember xml 형식과 json 형식으로 응답 받는 것 구현

---
+++ TeamMember/MyTeamController.java
@@ -0,0 +1,64 @@
+package my.spring.springedu;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import vo.TeamMemberVO;
+import vo.TeamVO;
+
+@Controller
+public class MyTeamController {
+	
+	
+	@RequestMapping(value = "/body/xml3/{id}", produces = "application/xml; charset=utf-8")
+	@ResponseBody
+	public TeamVO TeamMember1(@PathVariable String id) {
+		TeamVO vo = new TeamVO();
+		ArrayList<TeamMemberVO> list = new ArrayList<TeamMemberVO>();
+		TeamMemberVO my = new TeamMemberVO();
+		my.setName("정해림");
+		my.setNicName("지각쟁이");
+		list.add(my);
+		my = new TeamMemberVO();
+		my.setName("김수이");
+		my.setNicName("팀의 기둥");
+		list.add(my);
+		my = new TeamMemberVO();
+		my.setName("황지원");
+		my.setNicName("에이스");
+		list.add(my);
+		vo.setTeamName("각개격파");
+		vo.setTeamMember(list);
+		return vo;
+	}	
+	
+	
+	@RequestMapping(value = "/body/json5/{id}", produces = "application/json; charset=utf-8")
+	@ResponseBody
+	public TeamVO TeamMember2(@PathVariable String id) {
+		TeamVO vo = new TeamVO();
+		ArrayList<TeamMemberVO> list = new ArrayList<TeamMemberVO>();
+		TeamMemberVO my = new TeamMemberVO();
+		my.setName("정해림");
+		my.setNicName("지각쟁이");
+		list.add(my);
+		my = new TeamMemberVO();
+		my.setName("김수이");
+		my.setNicName("팀의 기둥");
+		list.add(my);
+		my = new TeamMemberVO();
+		my.setName("황지원");
+		my.setNicName("에이스");
+		list.add(my);
+		vo.setTeamName("각개격파");
+		vo.setTeamMember(list);
+		return vo;
+	}	
+	
+	
+
+}
